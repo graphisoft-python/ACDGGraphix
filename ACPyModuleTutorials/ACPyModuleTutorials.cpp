90ebd4a7bd389f8386f90f4dc441ced308c4b5e7
WangJun <445212619@qq.com>
2019-07-10 20:17:09 +0800

    添加AC-GS::UniString类型到Python-unicode类型映射头文件

--- ACPyModuleTutorials/ACPyModuleTutorials.cpp
+++ ACPyModuleTutorials/ACPyModuleTutorials.cpp
@@ -4,30 +4,52 @@
 #include "stdafx.h"
 
 
-#include<pybind11/pybind11.h>
-#include<pybind11/operators.h>
+#include <pybind11/pybind11.h>
+#include <pybind11/operators.h>
+#include <ACCast.h>
+
 
 #include "DGWImageCache.hpp"
 #include "DGUtility.hpp"
 
+
 namespace py = pybind11;
 
+class CastTest {
+public:
+	CastTest(GS::UniString &ustr) {
+		this->m_ustr = ustr;
+	}
+
+	GS::UniString &Get() {
+		return this->m_ustr;
+	}
+
+private:
+	GS::UniString m_ustr;
+};
 
 void load_dg_point(py::module m) {
-	py::class_<DG::Point> m_point(m, "Point");
-	m_point
-		.def(py::init<short, short>())
-		.def("Set", &DG::Point::Set)
-		.def("SetX", &DG::Point::SetX)
-		.def("GetX", &DG::Point::GetX)
-		.def("SetY", &DG::Point::SetY)
-		.def("GetY", &DG::Point::GetY)
-		.def("Offset", &DG::Point::Offset)
-		.def(py::self == py::self)
-		.def(py::self != py::self)
+	//py::class_<DG::Point> m_point(m, "Point");
+	//m_point
+	//	.def(py::init<short, short>())
+	//	.def("Set", &DG::Point::Set)
+	//	.def("SetX", &DG::Point::SetX)
+	//	.def("GetX", &DG::Point::GetX)
+	//	.def("SetY", &DG::Point::SetY)
+	//	.def("GetY", &DG::Point::GetY)
+	//	.def("Offset", &DG::Point::Offset)
+	//	.def(py::self == py::self)
+	//	.def(py::self != py::self)
+	//	;
+
+	py::class_< CastTest> c_cast(m, "CastTest");
+
+	c_cast
+		.def(py::init<GS::UniString &>())
+		.def("Get", &CastTest::Get)
 		;
 }
-
 
 PYBIND11_MODULE(ACPyModuleTutorials, m) {
 
