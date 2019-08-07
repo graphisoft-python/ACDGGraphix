0996eede2bea03082a9b01b72a73f777877ca41a
HaoRui <hr854164641@qq.com>
2019-08-07 10:48:03 +0800

    1、填补ListBoxUpdateEventContext遗漏的一个构造函数

--- DGGraphix/implementations/DGNativeContextFunc.hpp
+++ DGGraphix/implementations/DGNativeContextFunc.hpp
@@ -25,6 +25,7 @@ void load_UserItemUpdateNativeContext(py::module m) {
 void load_ListBoxUpdateEventContext(py::module m) {
 	py::class_<ListBoxUpdateEventContext, NativeContext>(m, "ListBoxUpdateEventContext")
 		.def(py::init<DG::ListBoxItemUpdateEvent &>())
+		.def(py::init<DG::ListBoxTabItemUpdateEvent &>())
 		//.def(py::init<>())
 		//.def(py::init<>())
 		;
